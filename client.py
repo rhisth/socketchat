a20a9e4b3f46cd749441fe710bf5e8b83c876b1e
rhisth <96009471+rhisth@users.noreply.github.com>
2023-10-16 18:51:11 +0000

    Create client.py

---
+++ client.py
@@ -0,0 +1,61 @@
+import socket
+import threading
+import os
+from time import sleep
+
+from config import hostname, port
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def listen(connection):
+    while True:
+        try:
+            data = connection.recv(1024)
+        except ConnectionResetError:
+            print("Оборвалось подключение с сервером.")
+            connection.close()
+            return
+        except ConnectionAbortedError:
+            print("Сервер закрыл подключение.")
+            connection.close()
+            return
+        if data:
+            print(data.decode())
+
+def input_message(connection):
+    while True:
+        message = input()
+        if message == "/close":
+            connection.close()
+            break
+        try:
+            connection.sendall(message.encode())
+        except ConnectionResetError:
+            clear()
+            return
+
+def input_name():
+    name = None
+    while not name:
+        name = input("Введите никнейм: ")
+    return name
+
+def main():
+    clear()
+    name = input()
+    client = socket.socket()
+    try:
+        client.connect((hostname, port))
+    except ConnectionRefusedError:
+        print("Не удалось подключиться к серверу.")
+        client.close()
+        return
+    client.send(name.encode())
+    print(f"Успешное подключение к серверу {hostname}:{port}.")
+    thread = threading.Thread(target=listen, args=(client,), daemon=True)
+    thread.start()
+    input_message(client)
+
+if __name__ == "__main__":
+    main()
